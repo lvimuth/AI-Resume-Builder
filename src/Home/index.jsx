7529dada2e509ba65286fc4c5e7ec7844b1de69b
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-11-02 00:03:07 +0400

    react router dom

---
+++ src/Home/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div>Home reass</div>;
+}
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SignInPage from "./auth/sign-in";
+import Home from "./Home";
+import Dashboard from "./dashboard";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  // default route
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+    ],
+  },
+  {
+    path: "/auth/sign-in",
+    element: <SignInPage />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
