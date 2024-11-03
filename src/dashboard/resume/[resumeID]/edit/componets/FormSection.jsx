0d23944f3ee08b6c2e91146e2c75d502d226bc63
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-11-03 16:48:00 +0400

    real time update personal details

--- src/dashboard/resume/[resumeID]/edit/componets/FormSection.jsx
+++ src/dashboard/resume/[resumeID]/edit/componets/FormSection.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import PersonalDetails from "./forms/PersonalDetails";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft, ArrowRight, LayoutGrid } from "lucide-react";
 
 function FormSection() {
-  return <div>FormSection</div>;
+  const [activeFormIndex, setActiveFormIndex] = useState(1);
+  const [enableNext, setEnableNext] = useState(false);
+  return (
+    <div>
+      {/* Personal Details */}
+      <div className="flex justify-between items-center">
+        <Button variant="outline" size="sm" className="flex gap-2">
+          <LayoutGrid />
+          Theme
+        </Button>
+        <div className="flex gap-2">
+          {activeFormIndex > 1 && (
+            <Button
+              variant="outline"
+              className=""
+              size="sm"
+              onClick={() => setActiveFormIndex(activeFormIndex - 1)}
+            >
+              <ArrowLeft />
+            </Button>
+          )}
+          <Button
+            disabled={!enableNext}
+            className="flex gap-2"
+            size="sm"
+            onClick={() => setActiveFormIndex(activeFormIndex + 1)}
+          >
+            Next <ArrowRight />
+          </Button>
+        </div>
+      </div>
+
+      {activeFormIndex == 1 ? (
+        <PersonalDetails enableNext={(v) => setEnableNext(v)} />
+      ) : null}
+
+      {/* Summary */}
+      {/* Experience */}
+      {/* Education */}
+      {/* Skills */}
+    </div>
+  );
 }
 
 export default FormSection;
