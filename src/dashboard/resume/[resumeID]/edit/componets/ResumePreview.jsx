0989c3f0a459d3c864823187328d50631c8d50cf
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-11-03 15:44:03 +0400

    preview section

--- src/dashboard/resume/[resumeID]/edit/componets/ResumePreview.jsx
+++ src/dashboard/resume/[resumeID]/edit/componets/ResumePreview.jsx
@@ -1,22 +1,32 @@
 import { ResumeInfoContext } from "@/context/ResumeContext";
 import React, { useContext } from "react";
 import PersonalDetailPreview from "./preview/PersonalDetailPreview";
+import SummaryPreview from "./preview/SummaryPreview";
+import ExperiencePreview from "./preview/ExperiencePreview";
+import EducationalPreview from "./preview/EducationalPreview";
+import SkillsPreview from "./preview/SkillsPreview";
 
 function ResumePreview() {
   const { resumeInfo, setResumeInfo } = useContext(ResumeInfoContext);
 
   console.log(resumeInfo);
   return (
-    <div>
+    <div
+      className="shadow-lg h-full p-14 border-t-[20px]"
+      style={{
+        borderColor: resumeInfo?.themeColor,
+      }}
+    >
       {/* Personal Details */}
       <PersonalDetailPreview resumeInfo={resumeInfo} />
       {/* Summary */}
-
+      <SummaryPreview resumeInfo={resumeInfo} />
       {/* Professional Experience */}
-
+      <ExperiencePreview resumeInfo={resumeInfo} />
       {/* Educational */}
-
+      <EducationalPreview resumeInfo={resumeInfo} />
       {/* Skills */}
+      <SkillsPreview resumeInfo={resumeInfo} />
     </div>
   );
 }
