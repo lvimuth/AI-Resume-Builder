a3bd22fc589ef480b85e2b78f72e31a1d86da5d3
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-11-03 14:31:11 +0400

    sample dummy data

--- src/dashboard/resume/[resumeID]/edit/index.jsx
+++ src/dashboard/resume/[resumeID]/edit/index.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import FormSection from "./componets/FormSection";
+import ResumePreview from "./componets/ResumePreview";
 
 function EditResume() {
   const params = useParams();
@@ -7,7 +9,15 @@ function EditResume() {
   useEffect(() => {
     console.log(params);
   }, []);
-  return <div>EditResume</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 p-10 gap-10">
+      {/* Form Section */}
+      <FormSection/>
+
+      {/* Preview Section */}
+      <ResumePreview/>
+    </div>
+  );
 }
 
 export default EditResume;
