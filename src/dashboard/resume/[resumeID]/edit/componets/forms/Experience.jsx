40c5dc00a4b986ddc6649de56536fad3077bbd27
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-11-04 23:53:09 +0400

    experience update

--- src/dashboard/resume/[resumeID]/edit/componets/forms/Experience.jsx
+++ src/dashboard/resume/[resumeID]/edit/componets/forms/Experience.jsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RichTextEditor from "../RichTextEditor";
 
 const formFiels = {
@@ -16,7 +16,24 @@ const formFiels = {
 function Experience() {
   const [experienceList, setExperienceList] = useState([formFiels]);
 
-  const handleChange = (index, event) => {};
+  const handleChange = (index, event) => {
+    const newEntries = experienceList.slice();
+    const { name, value } = event.target;
+    newEntries[index][name] = value;
+    setExperienceList(newEntries);
+  };
+
+  useEffect(() => {
+    console.log(experienceList);
+  }, [setExperienceList]);
+
+  const AddNewExperience = () => {
+    setExperienceList([...experienceList, formFiels]);
+  };
+
+  const RemoveExperience = () => {
+    setExperienceList((experienceList) => experienceList.slice(0, -1));
+  };
   return (
     <div>
       <div className="p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10">
@@ -24,7 +41,7 @@ function Experience() {
         <p>Add your previous experience</p>
         <div>
           {experienceList.map((item, index) => (
-            <div>
+            <div className="mt-5">
               <div className="grid grid-cols-2 gap-3 border p-3 rounded-lg">
                 <div className="col-span-2">
                   <label htmlFor="" className="text-xs ">
@@ -93,9 +110,23 @@ function Experience() {
           ))}
         </div>
         <div className="flex justify-between mt-5">
-          <Button variant="outline" className="text-primary">
-            + Add More Experience
-          </Button>
+          <div className="flex gap-2">
+            <Button
+              variant="outline"
+              onClick={AddNewExperience}
+              className="text-primary"
+            >
+              + Add More Experience
+            </Button>
+            <Button
+              variant="outline"
+              onClick={RemoveExperience}
+              className="text-primary"
+            >
+              - Remove
+            </Button>
+          </div>
+
           <Button>Save</Button>
         </div>
       </div>
